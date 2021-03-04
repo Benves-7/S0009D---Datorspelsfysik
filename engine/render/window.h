7ffef3810afc5f576c970ee276484c700f0116c7
Benjamin Vesterlund <benjamin_gb_W@hotmail.com>
2021-03-04 12:13:18 +0100

    reverted changes when a nicer implementation was found in the ImGui example was found..

--- engine/render/window.h
+++ engine/render/window.h
@@ -59,11 +59,6 @@ public:
 	void SetUiRender(const std::function<void()>& func);
 	/// set optional nanovg render function
 	void SetNanoVGRender(const std::function<void(NVGcontext *)> & func);
-	/// return window pointer
-	GLFWwindow* GetWindow()
-	{
-		return window;
-	}
 
 private:
 
