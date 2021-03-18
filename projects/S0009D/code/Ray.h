253ebe225f3a29df564b43e4ce8bb2a25fef4cb4
Benjamin Vesterlund <benjamin_gb_W@hotmail.com>
2021-03-18 10:38:15 +0100

    big refactor to make document more readable. added comments and found what was wrong with my functions, the intersection vertex data needs to be moved, rotated and scaled just as the object is.

--- projects/S0009D/code/Ray.h
+++ projects/S0009D/code/Ray.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "debug/debugShapes.h"
 #include "Square.h"
+#include "Plane.h"
 #include "MeshObject.h"
 #include <limits>
 
@@ -30,19 +31,20 @@ public:
 	{
 		return origin;
 	}
+	// Get a point i steps in the rays direction.
 	Vector4D getPointOnRay(int i = 0)
 	{
 		return origin + (direction * i);
 	}
 
-	// Goes through all planes in the vector. (returns the index of the closest intersect plane.)
-	int checkHit(vector<Square> planes)
+	// Goes through all squares in the vector. (returns the index of the closest intersect square.)
+	int bestHit(vector<Square> squares)
 	{
 		float closest = std::numeric_limits<float>::infinity();
 		int best = -1;
-		for (int i = 0; i < planes.size(); i++)
+		for (int i = 0; i < squares.size(); i++)
 		{
-			float dist = checkIfHitting(planes.at(i));
+			float dist = checkIfHitting(squares[i]);
 			if (dist < closest && dist != -1)
 			{
 				closest = dist;
@@ -53,51 +55,30 @@ public:
 			closest = -1;
 		return best;
 	}
-	// Check if a point is in individual square. (returns the distance to intersect point.) 
-	float checkIfHitting(Square plane)
+	// Check if intersection exists and is valid, then check if inside square. (returns the distance to intersect point.) 
+	float checkIfHitting(Square square)
 	{
-		IntersectData result = intersect(plane);
+		// Check if intersecting the plane
+		IntersectData result = intersect(Plane(square.getPosition(), square.getNormal()));
 		if (result.distance == -1)
 			return -1;
-
-		Vector4D a = result.point;
-		Vector4D b = plane.P1();
-		Vector4D c = plane.P2();
-		Vector4D d = plane.P3();
-		Vector4D e = plane.P4();
-
-		if ((Vector4D::dot(b, c - b) <= Vector4D::dot(a, c - b) && Vector4D::dot(a, c - b) <= Vector4D::dot(c, c - b)) &&
-			(Vector4D::dot(b, e - b) <= Vector4D::dot(a, e - b) && Vector4D::dot(a, e - b) <= Vector4D::dot(e, e - b)))
+		
+		// check if intersection is inside the square.
+		if (pointInSquare(result.point, square))
 		{
-			intersections.push_back(result);
-			DebugManager::getInstance().addShape(new DebugSphere(result.point, 0.05f));
+			DebugManager::getInstance().createSphere(result.point, 0.03, Vector4D(0,1,0));
 			return result.distance;
 		}
-		else
-		{
-			return -1;
-		}
-	}
-	// Check if ray intersect plane. (returns point of intersect and distance to intersect.) (IntersectData)
-	IntersectData intersect(Square plane)
-	{
-		Vector4D diff = origin - plane.getPosition();
-
-		float prod1 = diff.dot(plane.getNormal());
-		float prod2 = direction.dot(plane.getNormal());
-		float prod3 = prod1 / prod2;
-
-		// with the distance a point on the plane can be calculated.
-		return IntersectData((origin - direction * prod3), -prod3);
+		return -1;
 	}
 
 	// Goes through all objects in the vector. (return the index of the closest intersect object.)
-	int checkHit(vector<MeshObject> objects)
+	int bestHit(vector<MeshObject> objects)
 	{
 		vector<MeshObject> hits;
 		// Check if AABB is hit by the ray. add the object to temporary vector.
 		for (int i = 0; i < objects.size(); i++)
-			if (checkHit(objects[i].getAABB()) != -1)
+			if (bestHit(objects[i].getAABB()) != -1)
 				hits.push_back(objects[i]);
 
 		// Go deeper.
@@ -105,78 +86,112 @@ public:
 		int best = -1;
 		for (int i = 0; i < hits.size(); i++)
 		{
-			MeshInfo mi = hits[i].getMeshInfo();
-			Vector4D n1, n2, n3;
-			origin = Matrix4D::inverse(Matrix4D::getPositionMatrix(hits[i].getPosition())) * origin;
-			direction = Matrix4D::inverse(Matrix4D::getPositionMatrix(hits[i].getPosition())) * direction;
-			for (int j = 0; j < mi.indices.size(); j += 3)
+			float dist = checkIfHitting(hits[i]);
+			if (dist < closest && dist != -1)
 			{
-				n1[0] = mi.combinedBuffer[mi.indices[i + 0]].normal[0]; n1[1] = mi.combinedBuffer[mi.indices[i + 0]].normal[1]; n1[2] = mi.combinedBuffer[mi.indices[i + 0]].normal[2];
-				n2[0] = mi.combinedBuffer[mi.indices[i + 1]].normal[0]; n2[1] = mi.combinedBuffer[mi.indices[i + 1]].normal[1]; n2[2] = mi.combinedBuffer[mi.indices[i + 1]].normal[2];
-				n3[0] = mi.combinedBuffer[mi.indices[i + 2]].normal[0]; n3[1] = mi.combinedBuffer[mi.indices[i + 2]].normal[1]; n3[2] = mi.combinedBuffer[mi.indices[i + 2]].normal[2];
-
-				Vector4D normal = (n1 + n2 + n3) * (1.0f / 3.0f);
-				if (Vector4D::dot(normal, direction) < 0)
-				{
-					Vector4D pos1, pos2, pos3;
-					pos1[0] = mi.combinedBuffer[mi.indices[i+0]].pos[0]; pos1[1] = mi.combinedBuffer[mi.indices[i+0]].pos[1]; pos1[2] = mi.combinedBuffer[mi.indices[i+0]].pos[2];
-					pos2[0] = mi.combinedBuffer[mi.indices[i+1]].pos[0]; pos2[1] = mi.combinedBuffer[mi.indices[i+1]].pos[1]; pos2[2] = mi.combinedBuffer[mi.indices[i+1]].pos[2];
-					pos3[0] = mi.combinedBuffer[mi.indices[i+2]].pos[0]; pos3[1] = mi.combinedBuffer[mi.indices[i+2]].pos[1]; pos3[2] = mi.combinedBuffer[mi.indices[i+2]].pos[2];
-					Vector4D v2v1 = pos2 - pos1; Vector4D v3v2 = pos3 - pos2; Vector4D v1v3 = pos1 - pos3;
-					Vector4D pos = (pos1 + pos2 + pos3) * (1.0f / 3.0f);
-
-					IntersectData temp = intersect(Square(pos, Vector2D(1, 1), normal));
-					Vector4D PV0 = temp.point - pos1; Vector4D PV1 = temp.point - pos2; Vector4D PV2 = temp.point - pos3;
-					if (Vector4D::dot(normal, Vector4D::cross(v2v1, PV0)) > 0 &&
-						Vector4D::dot(normal, Vector4D::cross(v3v2, PV1)) > 0 &&
-						Vector4D::dot(normal, Vector4D::cross(v1v3, PV2)) > 0)
-					{
-						DebugManager::getInstance().createSphere(Matrix4D::getPositionMatrix(hits[i].getPosition()) * temp.point, 0.03);
-						best = i;
-					}
-				}
+				closest = dist;
+				best = i;
 			}
 		}
 		if (closest == std::numeric_limits<float>::infinity())
 			closest = -1;
 		return best;
 	}
-	float checkIfHitting(vector<Vector4D> points)
+	// Check if intersection exists and is valid, then check if inside triangle of mesh. (returns the distance to intersect point.)
+	float checkIfHitting(MeshObject object)
 	{
-		IntersectData result = intersect(points[0], points[1], points[2]);
-		if (pointInTriangle(result.point, points))
+		vector<IntersectData> temp;
+		int closest = numeric_limits<int>::infinity();
+		MeshInfo mi = object.getMeshInfo();
+		// Go through all the indicies in steps of threes.
+		for (int i = 0; i < mi.indices.size(); i += 3)
 		{
-			return result.distance;
+			// Step through the three vertices and extract the normals into Vector class.
+			Vector4D n1, n2, n3;
+			n1[0] = mi.combinedBuffer[mi.indices[i+0]].normal[0]; n1[1] = mi.combinedBuffer[mi.indices[i+0]].normal[1]; n1[2] = mi.combinedBuffer[mi.indices[i+0]].normal[2];
+			n2[0] = mi.combinedBuffer[mi.indices[i+1]].normal[0]; n2[1] = mi.combinedBuffer[mi.indices[i+1]].normal[1]; n2[2] = mi.combinedBuffer[mi.indices[i+1]].normal[2];
+			n3[0] = mi.combinedBuffer[mi.indices[i+2]].normal[0]; n3[1] = mi.combinedBuffer[mi.indices[i+2]].normal[1]; n3[2] = mi.combinedBuffer[mi.indices[i+2]].normal[2];
+			// the planes normal will be the three normals added together and divided by three.
+			Vector4D nor = (n1+n2+n3) * (1.0f / 3.0f);
+			// move and rotate and scale the normal acording to the objects new position and rotation. the position and the scale is not needed, since it is only a direction. (however it seems worth having still)
+			nor = Matrix4D::getPositionMatrix(object.getPosition()) * Matrix4D::getRotationMatrix(object.getDirection()) * Matrix4D::getScaleMatrix(object.getScale()) * nor;
+			if (Vector4D::dot(nor, direction) < 0)
+			{
+				// Step through the three vertices and extract the positions into Vector class.
+				Vector4D p1, p2, p3;
+				p1[0] = mi.combinedBuffer[mi.indices[i + 0]].pos[0]; p1[1] = mi.combinedBuffer[mi.indices[i + 0]].pos[1]; p1[2] = mi.combinedBuffer[mi.indices[i + 0]].pos[2];
+				p2[0] = mi.combinedBuffer[mi.indices[i + 1]].pos[0]; p2[1] = mi.combinedBuffer[mi.indices[i + 1]].pos[1]; p2[2] = mi.combinedBuffer[mi.indices[i + 1]].pos[2];
+				p3[0] = mi.combinedBuffer[mi.indices[i + 2]].pos[0]; p3[1] = mi.combinedBuffer[mi.indices[i + 2]].pos[1]; p3[2] = mi.combinedBuffer[mi.indices[i + 2]].pos[2];
+				// the planes normal will be the three positions added together and divided by three.
+				Vector4D pos = (p1 + p2 + p3) * (1.0f / 3.0f);
+				// move and rotate and scale the position acording to the objects new position and rotation. a point in space so all the matrixes are needed.
+				pos = Matrix4D::getPositionMatrix(object.getPosition()) * Matrix4D::getRotationMatrix(object.getDirection()) * Matrix4D::getScaleMatrix(object.getScale()) * pos;
+
+				// Check if intersecting the plane
+				IntersectData result = intersect(Plane(pos, nor));
+				if (result.distance == -1)
+					continue;
+				// Check if intersection is inside the square.
+				if (pointInTriangle(result.point, p1, p2, p3, nor))
+				{
+					// Instead of returning straight away, add it to a new list (safe guard to insure two matches can't be found. (head and ass of cat.) 
+					// (though this should never be the case when that normal should be in the wrong direction)
+					temp.push_back(result);
+				}
+			}
 		}
-		return -1;
+		for (int i = 0; i < temp.size(); i++)
+		{
+			// Find the closest match and return that distance.
+			DebugManager::getInstance().createSphere(temp[i].point, 0.005, Vector4D(0, 0, 1));
+			if (temp[i].distance < closest)
+			{
+				closest = temp[i].distance;
+			}
+		}
+		return closest;
 	}
-	IntersectData intersect(Vector4D A, Vector4D B, Vector4D C)
+
+	// Check if a point is inside the bounding box of three other points.
+	bool pointInTriangle(Vector4D p, Vector4D a, Vector4D b, Vector4D c, Vector4D normal)
 	{
-		Vector4D AB = (B - A); Vector4D AC = (C - A);
-		Vector4D N = Vector4D::cross(AB, AC);
+		Vector4D ab = b - a; Vector4D bc = c - b; Vector4D ca = a - c;
+		Vector4D ap = p - a; Vector4D bp = p - b; Vector4D cp = p - c;
 
-		float d = Vector4D::dot(direction, N);
-		if (d == 0)
-			return IntersectData(Vector4D(0, 0, 0, 0), -1);
-
-		float dist = Vector4D::dot((A - direction), N) / d;
-
-		return IntersectData((origin - direction * dist), -dist);
+		if (Vector4D::dot(normal, Vector4D::cross(ab, ap)) > 0 && Vector4D::dot(normal, Vector4D::cross(bc, bp)) > 0 && Vector4D::dot(normal, Vector4D::cross(ca, cp)) > 0)
+			return true;
+		return false;
 	}
-	bool pointInTriangle(Vector4D p, vector<Vector4D> points)
+	// Check if a point is inside the bounding box of a square object.
+	bool pointInSquare(Vector4D p, Square s)
 	{
-		points[0] -= p;
-		points[1] -= p;
-		points[2] -= p;
+		Vector4D b = s.P1();
+		Vector4D c = s.P2();
+		Vector4D d = s.P3();
+		Vector4D e = s.P4();
 
-		Vector4D u = Vector4D::cross(points[1], points[2]);
-		Vector4D v = Vector4D::cross(points[2], points[0]);
-		Vector4D w = Vector4D::cross(points[0], points[1]);
-
-		if (Vector4D::dot(u, v) < 0.0f && Vector4D::dot(u, w) < 0.0f)
-			return false;
-		return true;
+		if ((Vector4D::dot(b, c - b) <= Vector4D::dot(p, c - b) && Vector4D::dot(p, c - b) <= Vector4D::dot(c, c - b)) && 
+			(Vector4D::dot(b, e - b) <= Vector4D::dot(p, e - b) && Vector4D::dot(p, e - b) <= Vector4D::dot(e, e - b)))
+			return true;
+		return false;
 	}
+
+	// Check if ray intersect plane. (returns point of intersect and distance to intersect.) (IntersectData)
+	IntersectData intersect(Plane plane)
+	{
+		Vector4D diff = origin - plane.getPosition();
+
+		float prod1 = Vector4D::dot(plane.getNormal(), diff);
+		if (prod1 < 0)
+			return IntersectData(Vector4D(), -1);
+		float prod2 = Vector4D::dot(plane.getNormal(), direction);
+		if (prod2 == 0)
+			return IntersectData(Vector4D(), -1);
+		float prod3 = -(prod1 / prod2);
+
+		return IntersectData((origin + direction * prod3), prod3);
+	}
+
 private:
 	Vector4D origin, direction;
 	vector<IntersectData> intersections;
