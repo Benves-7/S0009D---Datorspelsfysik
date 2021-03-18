50dbfccf30b1460ff73ebefb97ddfa07c3189ad2
Benjamin Vesterlund <benjamin_gb_W@hotmail.com>
2021-03-18 10:33:03 +0100

    a mathematikal plane is now held in the plane class.

--- projects/S0009D/code/Plane.h
+++ projects/S0009D/code/Plane.h
@@ -1,1 +1,37 @@
 #pragma once
+#include "math.h"
+
+class Plane
+{
+public:
+	Plane(Vector4D pos, Vector4D dir)
+	{
+		position = pos;
+		normal = dir;
+	}
+	~Plane()
+	{
+
+	}
+
+	void setPosition(const Vector4D pos)
+	{
+		position = pos;
+	}
+	Vector4D& getPosition()
+	{
+		return position;
+	}
+
+	void setNormal(const Vector4D dir)
+	{
+		normal = dir;
+	}
+	Vector4D& getNormal()
+	{
+		return normal;
+	}
+
+private:
+	Vector4D position, normal;
+};
